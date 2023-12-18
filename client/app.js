c73de616f35575409e0c8cca36f927c4fac85dba
Adrian Manczak <manczakadrian@gmail.com>
2023-12-18 05:16:05 +0100

    server

--- client/app.js
+++ client/app.js
@@ -19,3 +19,37 @@ const login = event => {
         alert('Please, enter your username!');
     }
 };
+loginForm.addEventListener('submit', (event) => login(event));
+
+const sendMessage = event => {
+    event.preventDefault();
+    let messageContent = messageContentInput.value;
+    if (!messageContent.length) {
+        alert('Please, enter a message!');
+    }
+    else {
+        addMessage(userName, messageContent);
+        socket.emit('message', { author: userName, content: messageContent })
+        messageContentInput.value = '';
+    }
+};
+
+const addMessage = (author, content) => {
+    const message = document.createElement('li');
+    message.classList.add('message');
+    message.classList.add('message--received');
+    if (author === userName) message.classList.add('message--self');
+    if (author === "ChatBot") {
+        message.classList.add('message--bot');
+    }
+    message.innerHTML = `
+    <h3 class="message__author">${userName === author ? 'You' : author}</h3>
+    <div class="message__content">
+      ${content}
+    </div>
+  `;
+    messagesList.appendChild(message);
+    message.scrollIntoView({ behavior: "smooth", block: "end" });
+}
+
+addMessageForm.addEventListener('submit', (event) => sendMessage(event));
